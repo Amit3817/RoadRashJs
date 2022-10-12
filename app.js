fe08687d7fb1f27a3bc0d9976b2b05b6e89d479e
Tushar <tusharc20001@gmail.com>
2022-10-12 15:27:37 +0530

    Tried adding collision

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var bike = document.getElementById('bike')
-var bikespeed = 0
+var bikespeed = 1
 var bike1speed = 100
 var bike2speed = 90
 var bike3speed = 85
 var bike4speed = 65
+var e = 0
 var bikeright = parseInt(window.getComputedStyle(bike).getPropertyValue("right"))
 var screen = document.getElementById('screen')
 var bike1 = document.getElementById('bike-1')
@@ -25,13 +26,14 @@ document.onkeydown = function (e) {
         bikeright -= 20;
         bike.style.right = bikeright + "px"
         var imgsrc = document.getElementById('bike')
-        imgsrc.style.backgroundImage = "url(./img/mainbikeright.png)"
+        imgsrc.style.transform = "rotate(20deg)"
     }
     else if (e.keyCode == 37) {
         bikeright += 10;
         bike.style.right = bikeright + "px"
         var imgsrc = document.getElementById('bike')
-        imgsrc.style.backgroundImage = "url(./img/mainbikeleft.png)"
+        // imgsrc.style.backgroundImage = "url(./img/mainbikeleft.png)"
+        imgsrc.style.transform = "rotate(-20deg)"
     }
     else if (e.keyCode == 38) {
         if (bikespeed < 20) {
@@ -45,9 +47,10 @@ document.onkeydown = function (e) {
         bike1btm = parseInt(window.getComputedStyle(bike2).getPropertyValue("bottom"))
         bike2.style.bottom = (bike1btm - bikespeed) + "px"
         a += 0.5;
-        console.log(parseInt(a));
+        // console.log(parseInt(a));
     }
     else if (e.keyCode == 40) {
+
         if (bikespeed > 0)
             bikespeed--;
         // console.log(bikespeed);
@@ -61,14 +64,14 @@ document.onkeydown = function (e) {
 setInterval(() => {
     a += 0.2
     var tot_dis = document.getElementById('dis')
-    tot_dis.innerHTML = parseInt(a);
+    tot_dis.innerHTML = (parseInt(a)) + "m";
 }, 100);
 var test = document.getElementById('screen')
 var test1 = test.offsetWidth;
 // console.log("erqer" + test1);
 document.onkeyup = function (e) {
     var imgsrc = document.getElementById('bike')
-    imgsrc.style.backgroundImage = "url(./img/mainbike.png)"
+    imgsrc.style.transform = "rotate(0deg)"
 }
 var dec = setInterval(() => {
     if (bikespeed > 0) {
@@ -77,25 +80,53 @@ var dec = setInterval(() => {
     }
 }, 500);
 function addcar() {
-    var car = document.createElement("div");
-    car.classList.add("car")
-    var carleft = parseInt(window.getComputedStyle(car).getPropertyValue("left"))
-    car.style.left = (Math.floor(Math.random() * 300) + 340) + "px"
+    var randomno = Math.random();
+    if (randomno < 0.25) {
+        var car = document.createElement("div");
+        car.classList.add("car")
+        car.style.left = 350 + "px"
+    }
+    else if (randomno <= 0.5) {
+        var car = document.createElement("div");
+        car.classList.add("car")
+        car.style.left = 420 + "px"
+    }
+    else if (randomno <= 0.75) {
+        var car = document.createElement("div");
+        car.classList.add("car")
+        car.style.left = 490 + "px"
+    }
+    else {
+        var car = document.createElement("div");
+        car.classList.add("car")
+        car.style.left = 560 + "px"
+    }
     screen.appendChild(car)
-    setTimeout(addcar, 2000 - 30 * bikespeed);
+    setTimeout(addcar, 2500 - 30 * bikespeed);
 };
 addcar();
 var movecars = setInterval(() => {
     var cars = document.getElementsByClassName("car");
-    if (cars != undefined) {
-        for (var i = 0; i < cars.length; i++) {
-            var car = cars[i];
-            var cartop = parseInt(window.getComputedStyle(car).getPropertyValue("top"))
-            car.style.top = cartop + (25 + 2.5 * bikespeed) + "px"
+    // if (cars != undefined) {
+    for (var i = 0; i < cars.length; i++) {
+        var car = cars[i];
+        var cartop = parseInt(window.getComputedStyle(car).getPropertyValue("top"))
+        var carbtm = parseInt(window.getComputedStyle(car).getPropertyValue("bottom"))
+        var carright = parseInt(window.getComputedStyle(car).getPropertyValue("right"))
+        var bikeright = parseInt(window.getComputedStyle(bike).getPropertyValue("right"))
+        car.style.top = cartop + (1 + parseInt(bikespeed / 4)) + "px"
+        var biketop = parseInt(window.getComputedStyle(bike).getPropertyValue("top"));
+        if ((carbtm >145&&carbtm<155)&&((Math.abs(carright - bikeright))>10)&&((Math.abs(carright - bikeright))<70)) {
+                e++;
+                var ii = document.getElementById("test")
+            ii.innerHTML = (e) + bikeright;
         }
+        // console.log(bikeright);
+        // console.log(carright);
     }
-
-}, 120);
+    // console.log(carright);
+    // }
+}, 5);
 var movebike1vertical = setInterval(() => {
     var bike1 = document.getElementById('bike-1')
     bike1btm = parseInt(window.getComputedStyle(bike1).getPropertyValue("bottom"))
@@ -109,6 +140,7 @@ var movebike2vertical = setInterval(() => {
     bike2.style.bottom = (bike1btm + 1) + "px"
 }, 5);
 var movebike1horizontal = setInterval(() => {
+    var bike1left = parseInt(window.getComputedStyle(bike1).getPropertyValue("left"))
     var pos = 0;
     var bike1hor = Math.floor(Math.random() * 30);
     var bike1right = parseInt(window.getComputedStyle(bike1).getPropertyValue("right"))
@@ -119,14 +151,15 @@ var movebike1horizontal = setInterval(() => {
     else {
         pos = 0;
     }
-    if (pos) {
+    if (pos && (bike1left > 380)) {
         bike1.style.right = (bike1right + bike1hor) + "px";
     }
-    else {
+    else if (!pos && (bike1left < 560)) {
         bike1.style.right = (bike1right - bike1hor) + "px";
     }
 }, 500);
 var movebike2horizontal = setInterval(() => {
+    var bike2left = parseInt(window.getComputedStyle(bike2).getPropertyValue("left"))
     var pos = 0;
     var bike2hor = Math.floor(Math.random() * 50);
     var bike2right = parseInt(window.getComputedStyle(bike2).getPropertyValue("right"))
@@ -137,10 +170,10 @@ var movebike2horizontal = setInterval(() => {
     else {
         pos = 0;
     }
-    if (pos) {
+    if (pos && (bike2left > 380)) {
         bike2.style.right = (bike2right + bike2hor) + "px";
     }
-    else {
+    else if (!pos && (bike2left < 560)) {
         bike2.style.right = (bike2right - bike2hor) + "px";
     }
 }, 700);
